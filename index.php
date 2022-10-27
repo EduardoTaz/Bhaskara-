17b798d3d802b339191b199a872aa47b2c0fd9c6
Eduardo Taz <edutazoi21@hotmail.com>
2022-10-27 17:17:31 -0300

    primeiro commit

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fórmula de Bhaskara</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+    <header>
+        <p id="titulo">Fórmula de Bhaskara</p>
+            <nav>
+                <li>
+                    <li><a href="#">Início</a></li>
+                    <li><a href="#">Vídeos</a></li>
+                    <li><a href="#">Exercícios</a></li>
+                    <li><a href="#">História</a></li>
+                </li>
+            </nav>
+    </header>
+
+    <main>
+
+        <article>
+            <p class="titulomain">Valores de A, B e C</p>
+
+            <p class="textomain">2x + 8x - 4</p>
+            <p class="textomain">A = 2; B = 8; C = -4</p>
+
+            <p class="titulomain">Delta</p>
+            <p class="textomain">Δ = 2b – 4ac</p>
+            <p class="textomain">Δ = 2.8 - 4.2.(-4)</p>
+            <p class="textomain">Δ = 16 - 32</p>
+            <p class="textomain">Δ = 16</p>
+
+            <hr>
+
+            <p class="titulomain">Bhaskara</p>
+            <p class="textomain">x = -b +- √Δ / 2a</p>
+            <p class="textomain">x = -8 +- √16 / 2.2</p>
+            <p class="textomain">x = -8 +- 4 / 4</p>
+            <p class="textomain">x1 = -8 + 4 / 4 = -1</p>
+            <p class="textomain">x2 = -8 -4 / 4 = -3</p>
+        </article>
+
+    </main>
+
+    <footer>
+     
+    </footer>
+
+</body>
+</html>
